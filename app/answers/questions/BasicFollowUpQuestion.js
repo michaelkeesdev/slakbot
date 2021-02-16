3f2a533fef8107e2513bb259003a1f816d4f64b3
Joachim Geerts <jg@syntegro.be>
2021-02-16 09:42:30 +0100

    fix idk

---
+++ app/answers/questions/BasicFollowUpQuestion.js
@@ -0,0 +1,20 @@
+export const BASIC_FOLLOWUP_TRIGGER = [
+    "?",
+    "Mag ik iets vragen?",
+    "Vraagje"
+  ];
+
+export const BASIC_FOLLOWUP_QUESTION = [
+    "Ja?",
+    "Wat is er?",
+    "Zeg maar?",
+    "Stel u vraagt slet",
+    "Wa iest",
+    "Yow",
+    "Mhm?",
+    "Updog",
+    "Wa moete hebbe",
+    "Laat mij gerust",
+    "Hoe kan ik u van dienst zijn?",
+    "Dit is Maggie-Hotline, hoe kan ik u helpen?"
+  ];

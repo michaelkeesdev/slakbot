058bdc08391eb4a8244960d0cf1c7d1fd07df08c
Michael Kees <michaelkees@MacBook-Pro.local>
2021-02-17 13:04:58 +0100

    feature/weather

--- app/services/weather/WeatherService.js
+++ app/services/weather/WeatherService.js
@@ -12,7 +12,6 @@ class WeatherService {
         city ? city : "Antwerp"
       }&appid=a56e4906698ac4909f50c2b0f9f72647&units=metric&lang=nl`
     );
-    console.log(response)
     return `${response?.weather[0]?.description}, ${response?.main?.temp } °C in ${response?.name}`;
   };
 }

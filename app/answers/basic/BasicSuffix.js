9eb7bf1e89fd6a8964b7d4352fbd641b2e6c82c7
Michael Kees <michaelkees@MacBook-Pro.local>
2021-02-23 09:45:27 +0100

    added emojis

--- app/answers/basic/BasicSuffix.js
+++ app/answers/basic/BasicSuffix.js
@@ -1,10 +1,7 @@
 export const BASIC_SUFFIX = [
     "Zucht.",
-    ":simple_smile:",
-    ":joy:",
     "Allee tot ziens nu he.",
     "De groeten.",
-    ":eggplant:",
     "Maggie out :v: ",
     "Over en out",
     "Ik moet nu trouwens even weg, tot ziens!",
@@ -12,10 +9,5 @@ export const BASIC_SUFFIX = [
     "Hoer",
     "Laat mij nu eens me rust.",
     "Ik ben nu even moe, ik ga een dutje doen",
-    ":simpeler:",
-    ":v:",
-    ":hoh:",
-    ":yum:",
-    ":sunglasses:",
     "lit fam yolo sweg yuuuuuuuw"
 ];

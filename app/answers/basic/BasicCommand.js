972c1b937cc6098b1c10fc3955edc95b2d36e140
Joachim Geerts <joachim.geerts@gmail.com>
2021-02-23 15:15:48 +0100

    flip array pro cood

--- app/answers/basic/BasicCommand.js
+++ app/answers/basic/BasicCommand.js
@@ -2,6 +2,7 @@ export const BASIC_COMMAND = [
     "en zegt dat hij moet",
     "en zegt dat em moet",
     "en zegt hem dat",
+    "en zeg dat hij moet",
     "en zeg dat em moet",
     "en zegt dat",
     "en zeg hem",

a9aeca267b5551b77443af12abc04f678ae132a0
Michael Kees <michaelkees@MacBook-Pro.local>
2021-02-23 11:02:36 +0100

    dec

--- app/services/decision/DecisionService.js
+++ app/services/decision/DecisionService.js
@@ -9,8 +9,8 @@ class DecisionService {
 
     makeDecision(text) {
         if (this.needsToDecide(text)) {
-            let ALL_PID = Math.floor(Math.random() * ALL_PID);
-            if(ALL_PID === 1) {
+            let and = Math.floor(Math.random() * ALL_PID);
+            if(and === 1) {
                 return text.replace(" of ", " en ");
             } else {
                 return sample(text.split(" of "))

ee47c6a3950e040511a0fa82ca8b03a0e64e82c2
Joachim Geerts <joachim.geerts@gmail.com>
2021-02-24 15:34:54 +0100

    howservice

--- app/answers/How.js
+++ app/answers/How.js
@@ -4,7 +4,7 @@ export const HOW_TRIGGER = [
 
 export const HOW_ANSWER = [
     "Je moet een %noun% %verb%",
-    "Met de %noun% van %user%",
+    "Met %noun% van %user%",
     "Door %noun% van %user% te %verb%",
     "Met de hulp van %user%",
     "Ik weet het niet. %user% weet jij het?",

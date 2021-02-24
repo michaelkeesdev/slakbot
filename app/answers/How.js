43efde64730f9f4051b6a76b5406057eea8c36c0
Joachim Geerts <joachim.geerts@gmail.com>
2021-02-24 15:30:37 +0100

    howservice

--- app/answers/How.js
+++ app/answers/How.js
@@ -6,7 +6,6 @@ export const HOW_ANSWER = [
     "Door %noun% van %user% te %verb%",
     "Met de hulp van %user%",
     "Ik weet het niet. %user% weet jij het?",
-    "Hoe, hoe, ge zijt zelf een hoer",
-    "Weet ni"
+    "Weet ni",
   ];
   

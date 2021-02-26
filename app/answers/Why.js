1314aba2346b6a7e51771bb853b4abb1ba78568c
joachimgeerts <joachim.geerts@gmail.com>
2021-02-26 16:36:22 +0100

    add second noun to why

--- app/answers/Why.js
+++ app/answers/Why.js
@@ -1,9 +1,9 @@
 export const WHY_TRIGGER = ["waarom", "reden", "hoezo"];
 
 export const WHY_ANSWERS = [
-  "Omdat %noun% moet %verb%",
-  "Omdat %noun% zal %verb%",
-  "Om de reden dat %noun% van %user% iets moet %verb%",
+  "Omdat %noun1% moet %verb%",
+  "Omdat %noun1% zal %verb%",
+  "Om de reden dat %noun1% van %user% %noun2% moet %verb%",
   "Doordat %noun% van %user% zal %verb%",
   "Vermits %user% moet %verb%",
   "Goeie vraag. %user% weet jij het?",

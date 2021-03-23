e54ab25d2eb34eb4f4dfecd5283ba231df69c834
Michael Kees <michaelkees@MacBook-Pro.local>
2021-03-23 11:27:34 +0100

    uppercase

--- app/services/MaggieMond.js
+++ app/services/MaggieMond.js
@@ -1,10 +1,9 @@
-
-import { sample } from 'lodash';
+import { sample } from "lodash";
 
 import { BASIC_FOLLOWUP_QUESTION } from "./../answers/questions/BasicFollowUpQuestion";
 
 import { BYE_ANSWER } from "./../answers/Bye";
-import { COUNTRY_LIST } from "./../answers/Countries"
+import { COUNTRY_LIST } from "./../answers/Countries";
 import { GOODMORNING_ANSWER } from "./../answers/Goodmorning";
 import { HOW_ANSWER } from "./../answers/How";
 import { JOKE_ANSWER } from "./../answers/Joke";
@@ -14,31 +13,30 @@ import { WHEN_ANSWER } from "./../answers/When";
 import { WHERE_ANSWER } from "./../answers/Where";
 import { SORRY_ANSWER } from "./../answers/Sorry";
 
-import { BasicAnweringService } from "./answering/BasicAnsweringService"
+import { BasicAnweringService } from "./answering/BasicAnsweringService";
 import { UserService } from "./user/UserService";
-import { BirthdayService } from "./user/BirthdayService"
+import { BirthdayService } from "./user/BirthdayService";
 import { NewsService } from "./news/NewsService";
 import { NineGagService } from "./9gag/9gagService";
 import { FoodService } from "./food/FoodService";
 import { YoutubeService } from "./google/YoutubeService";
 import { DecisionService } from "./answering/DecisionService";
-import { EuroMillionsService } from './gambling/EuroMillionsService';
-import { WeatherService } from './weather/WeatherService';
-import { TimeService } from './time/TimeService';
-import { HttpClient } from '../../httpClient';
+import { EuroMillionsService } from "./gambling/EuroMillionsService";
+import { WeatherService } from "./weather/WeatherService";
+import { TimeService } from "./time/TimeService";
+import { HttpClient } from "../../httpClient";
 import { DeviantArtService } from "./deviantart/deviantArtService";
-import { HowMuchService } from './HowMuchService';
-import { MONTH_ANSWERS } from '../answers/Month';
-import { HowService } from './hoe/HowService';
-import { WhyService } from './why/WhyService';
-import { NOUNS } from '../answers/words/RandomNouns';
-import { ImageRecognitionService } from './recognition/ImageRecognition';
-import { MONOLOGUE } from '../answers/Monologue';
-import { HOER } from '../answers/Hoer';
-import { TagService } from './user/TagService';
-import { TokenizerService } from './tokenizer/Tokenizer';
-import { MolService } from './tv/molService';
-
+import { HowMuchService } from "./HowMuchService";
+import { MONTH_ANSWERS } from "../answers/Month";
+import { HowService } from "./hoe/HowService";
+import { WhyService } from "./why/WhyService";
+import { NOUNS } from "../answers/words/RandomNouns";
+import { ImageRecognitionService } from "./recognition/ImageRecognition";
+import { MONOLOGUE } from "../answers/Monologue";
+import { HOER } from "../answers/Hoer";
+import { TagService } from "./user/TagService";
+import { TokenizerService } from "./tokenizer/Tokenizer";
+import { MolService } from "./tv/MolService";
 
 const basicAnweringService = new BasicAnweringService();
 
@@ -64,57 +62,130 @@ const tagService = new TagService();
 const molService = new MolService();
 
 class MaggieMond {
-  askBasicFollowUpQuestion() { return sample(BASIC_FOLLOWUP_QUESTION); }
+  askBasicFollowUpQuestion() {
+    return sample(BASIC_FOLLOWUP_QUESTION);
+  }
 
-  giveBasicAnswer() { return basicAnweringService.buildAnswerPhrase(); }
-  sayBye() { return sample(BYE_ANSWER); }
-  sayBirthDay(text) { return birthdayService.getBirthday(text); }
-  sayCountry() { return sample(COUNTRY_LIST).name; }
-  sayGoodMorning() { return sample(GOODMORNING_ANSWER); }
-  sayHow() { return howService.getHowAnswer(); }
-  sayHowMuch() { return  howMuchService.giveNumber() }
-  sayHowYouDoing() { return basicAnweringService.buildAnswerToHowYouDoingPhrase(); }
-  sayJoke() { return sample(JOKE_ANSWER); }
-  sayThanks() { return sample(THANKS_ANSWER); }
-  sayWeetje() { return sample(WEETJES_ANSWER); }
-  sayWhen() { return sample(WHEN_ANSWER); }
-  sayWhere() { return sample(WHERE_ANSWER); }
-  sayMonth() { return sample(MONTH_ANSWERS); }
-  sayWhy() { return whyService.getWhyAnswer() }
-  sayWhat() { return sample(NOUNS) }
+  giveBasicAnswer() {
+    return basicAnweringService.buildAnswerPhrase();
+  }
+  sayBye() {
+    return sample(BYE_ANSWER);
+  }
+  sayBirthDay(text) {
+    return birthdayService.getBirthday(text);
+  }
+  sayCountry() {
+    return sample(COUNTRY_LIST).name;
+  }
+  sayGoodMorning() {
+    return sample(GOODMORNING_ANSWER);
+  }
+  sayHow() {
+    return howService.getHowAnswer();
+  }
+  sayHowMuch() {
+    return howMuchService.giveNumber();
+  }
+  sayHowYouDoing() {
+    return basicAnweringService.buildAnswerToHowYouDoingPhrase();
+  }
+  sayJoke() {
+    return sample(JOKE_ANSWER);
+  }
+  sayThanks() {
+    return sample(THANKS_ANSWER);
+  }
+  sayWeetje() {
+    return sample(WEETJES_ANSWER);
+  }
+  sayWhen() {
+    return sample(WHEN_ANSWER);
+  }
+  sayWhere() {
+    return sample(WHERE_ANSWER);
+  }
+  sayMonth() {
+    return sample(MONTH_ANSWERS);
+  }
+  sayWhy() {
+    return whyService.getWhyAnswer();
+  }
+  sayWhat() {
+    return sample(NOUNS);
+  }
 
-  sayRandomUser() { return userService.getRandomUserRandomName(); }
-  tagUser(text) { return tagService.tagUserAndAddTextCommand(text); }
+  sayRandomUser() {
+    return userService.getRandomUserRandomName();
+  }
+  tagUser(text) {
+    return tagService.tagUserAndAddTextCommand(text);
+  }
 
-  showMeme() { return ninegagService.get9gagBasic(); }
-  showGirl() { return ninegagService.get9gagGirl(); }
-  showCosplay() { return deviantArtService.getDeviant('cosplay'); }
-  showNsfw() { return deviantArtService.getDeviant('nude-art'); }
-  sayTime(text) { return timeService.getRandomTime(text); }
-  saySluip() { return youtubeService.getSluip(); }
-  sayRandomYoutube(text) { return youtubeService.getRandomYoutube(text); }
-  sayExactYoutube(text) { return youtubeService.getExactYoutube(text); }
-  readTheNews() { return newsService.getNewsPosts(); }
-  sayCurrentWeather(city) { return weatherService.getCurrentWeather(city); }
-  sayCurrentWeatherPollution(city) { return weatherService.getCurrentPolution(city); }
-  sayForecastWeather(city) { return weatherService.getAllWeatherInfo(city); }
-  sayFood(suggest) { return foodService.getRecaipie(suggest); }
+  showMeme() {
+    return ninegagService.get9gagBasic();
+  }
+  showGirl() {
+    return ninegagService.get9gagGirl();
+  }
+  showCosplay() {
+    return deviantArtService.getDeviant("cosplay");
+  }
+  showNsfw() {
+    return deviantArtService.getDeviant("nude-art");
+  }
+  sayTime(text) {
+    return timeService.getRandomTime(text);
+  }
+  saySluip() {
+    return youtubeService.getSluip();
+  }
+  sayRandomYoutube(text) {
+    return youtubeService.getRandomYoutube(text);
+  }
+  sayExactYoutube(text) {
+    return youtubeService.getExactYoutube(text);
+  }
+  readTheNews() {
+    return newsService.getNewsPosts();
+  }
+  sayCurrentWeather(city) {
+    return weatherService.getCurrentWeather(city);
+  }
+  sayCurrentWeatherPollution(city) {
+    return weatherService.getCurrentPolution(city);
+  }
+  sayForecastWeather(city) {
+    return weatherService.getAllWeatherInfo(city);
+  }
+  sayFood(suggest) {
+    return foodService.getRecaipie(suggest);
+  }
 
-  speakDecision(text) { return decisionService.makeDecision(text) }
+  speakDecision(text) {
+    return decisionService.makeDecision(text);
+  }
 
-  tellNextEuroMillionsDraw() { return euroMillionsService.getNextDraw(); }
-  recognize(imageUrl) { return recognitionService.get(imageUrl) }
+  tellNextEuroMillionsDraw() {
+    return euroMillionsService.getNextDraw();
+  }
+  recognize(imageUrl) {
+    return recognitionService.get(imageUrl);
+  }
 
-  saySorry() {return sample(SORRY_ANSWER)}
+  saySorry() {
+    return sample(SORRY_ANSWER);
+  }
 
-  sayMonologue() { return sample(MONOLOGUE) }
-  sayHoer() { return sample(HOER) }
-  sayMolName() { return molService.giveRandomName(); }
+  sayMonologue() {
+    return sample(MONOLOGUE);
+  }
+  sayHoer() {
+    return sample(HOER);
+  }
+  sayMolName() {
+    return molService.giveRandomName();
+  }
 }
 
 export { MaggieMond };
-
-
-
-
-

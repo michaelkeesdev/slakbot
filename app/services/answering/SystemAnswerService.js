f30fe76efd04e0bfd09e47bb6eceeb64b2c564a3
Michael Kees <michaelkees@MacBook-Pro.local>
2021-03-23 11:12:53 +0100

    explanation

--- app/services/answering/SystemAnswerService.js
+++ app/services/answering/SystemAnswerService.js
@@ -1,45 +1,48 @@
-import { sample } from 'lodash';
-import { SYSTEM_ANSWER_PREFIX, SYSTEM_ANSWER, SYSTEM_ANSWER_SUFFIX } from '../../answers/basic/ExplanationAnswer';
-import { NOUNS } from '../../answers/words/RandomNouns';
-import { StringUtil } from './../../util/StringUtil';
-import { UserService } from './../user/UserService';
+import { sample } from "lodash";
+import {
+  SYSTEM_ANSWER_PREFIX,
+  SYSTEM_ANSWER,
+  SYSTEM_ANSWER_SUFFIX,
+} from "./../../answers/basic/ExplanationAnswer";
+import { NOUNS } from "../../answers/words/RandomNouns";
+import { UserService } from "./../user/UserService";
 
 const SUFFIX_PID = 8;
 
 const userService = new UserService();
 
 class SystemAnswerService {
-    appendSystemAnswer(responseBuilder) {
-        let addOpinionSuffix = Math.floor(Math.random() * SUFFIX_PID);
+  appendSystemAnswer(responseBuilder) {
+    let addOpinionSuffix = Math.floor(Math.random() * SUFFIX_PID);
 
-        responseBuilder.appendFullStopIfNone().append(" ").append(sample(SYSTEM_ANSWER_PREFIX));
-        responseBuilder.append(" ").append(this.getSystemAnswer());
+    responseBuilder
+      .appendFullStopIfNone()
+      .append(" ")
+      .append(sample(SYSTEM_ANSWER_PREFIX));
+    responseBuilder.append(" ").append(this.getSystemAnswer());
 
-        if (addOpinionSuffix == 1) {
-            responseBuilder.append(" ").append(sample(SYSTEM_ANSWER_SUFFIX));
-
-        }
-
-        return responseBuilder;
+    if (addOpinionSuffix == 1) {
+      responseBuilder.append(" ").append(sample(SYSTEM_ANSWER_SUFFIX));
     }
 
-    getSystemAnswer() {
-        let noun = sample(NOUNS);
-        let user = userService.getRandomUserRandomName();
+    return responseBuilder;
+  }
 
-        let opinionContents = {
-            "%noun%": noun,
-            "%user%": user,
-        }
+  getSystemAnswer() {
+    let noun = sample(NOUNS);
+    let user = userService.getRandomUserRandomName();
 
-        let opinion = sample(SYSTEM_ANSWER).replace(/%\w+%/g, function (all) {
-            return opinionContents[all] || all;
-        });
+    let opinionContents = {
+      "%noun%": noun,
+      "%user%": user,
+    };
 
-        return opinion;
-    }
+    let opinion = sample(SYSTEM_ANSWER).replace(/%\w+%/g, function (all) {
+      return opinionContents[all] || all;
+    });
+
+    return opinion;
+  }
 }
 
 export { SystemAnswerService };
-
-
--- app/services/tv/MolService.js
+++ app/services/tv/MolService.js
@@ -2,9 +2,9 @@ import { sample } from "lodash";
 import { MOL_NAMES } from "./../../answers/mol/Names";
 
 class MolService {
-    giveRandomName() {
-        return sample(MOL_NAMES);
-    }
+  giveRandomName() {
+    return sample(MOL_NAMES);
+  }
 }
 
-export {MolService};
+export { MolService };

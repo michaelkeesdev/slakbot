be3df3f91a9a949ebc8ff9bcbda245f6188380ff
Joachim Geerts <joachim.geerts@gmail.com>
2021-08-12 11:25:40 +0200

    weet ni meer

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ discordApp.post('/interactions', verifyKeyMiddleware('f79402272be9d36eb834d5c936
 
 client.login(process.env.DISCORD_TOKEN);
 
-discordApp.listen(80, function () {
+discordApp.listen(process.env.PORT || 8080, function () {
   client.once('ready', () => {
     console.log('⚡️ Discord Ready!');
   });

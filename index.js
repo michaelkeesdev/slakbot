9e878f7275a6cf9f6d16756907adebfb1eab8773
joachim012 <90557156+joachim012@users.noreply.github.com>
2021-11-22 15:08:36 +0100

    zwijg fix

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.event("app_mention", async ({ context, event }) => {
 
 (async () => {
   await app.start(process.env.PORT || 8080);
-  console.log("mopje", await maggieSlack.getMentionResponse("zwijg", null, null, "U92KLC4CX"));
+  //console.log("mopje", await maggieSlack.getMentionResponse("zwijg", null, null, "U92KLC4CX"));
   console.log("⚡️ Slakbot is running!");
 })();
 

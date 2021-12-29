90b39fa2580b1e011936241f531951e19ac47751
joachim012 <90557156+joachim012@users.noreply.github.com>
2021-12-29 14:21:52 +0100

    Random mood

---
+++ app/answers/basic/Prefix.js
@@ -0,0 +1,38 @@
+export const BASIC_PHRASE = [
+  "Even denken...",
+  "Ah hm...",
+  "Euh...",
+  "Mhm...",
+];
+
+export const BASIC_PHRASE_BAD_MOOD = [
+  "Lachwekkend.",
+  "Triest.",
+  "Zielige steen.",
+  "k",
+  "Ok.",
+  "Wat een vraag?",
+  "Pfff..",
+  "Vermoeiend?",
+  "Gy bent dom?",
+  "Kunt gij eigenlijk iets?",
+  "Zou beter opkankeren. En..",
+  "Tyfushoer.",
+  "Kalm he",
+  "Halloooooooo.",
+  "Hey hoi.",
+  "Eigenlijk boeit het mij niet."
+];
+
+export const BASIC_PHRASE_GOOD_MOOD = [
+  "Blij dat je mij stuurt xxx.",
+  "Aanwezig.",
+  "XD",
+  "Haha.",
+  "Lol.",
+  "Hoh.",
+  ":')",
+  ":D",
+  "Hihi.",
+  "Hallooo!"
+];
---
+++ app/answers/basic/Suffix.js
@@ -0,0 +1,41 @@
+export const SUFFIX = [
+    "Allee tot ziens nu he.",
+    "De groeten.",
+    "Maggie out :v: ",
+    "Over en out",
+    "Ff weg nu",
+    "Ff moe nu, dutje",
+    "ff kaf",
+    "Hoogachtend, Maggie.",
+    "Tot de volgende.",
+    "Doeg",
+    "Houdoe",
+    "Nu ben ik wel echt weg",
+    "Want de bel doet het niet."
+];
+
+export const SUFFIX_BAD_MOOD = [
+    "Slet.",
+    "Hoer.",
+    "En laat me nu doen",
+    "En zwijg nu?",
+    "En als ge mij nog 1 keer tagt krijgt ge lappen.",
+    "En gy bent een %adjective% %scheld% overigens.",
+    "En dans",
+    "Zucht.",
+    "Dag.",
+    "Laat me doen %scheld%",
+    "%scheld%"
+];
+
+export const SUFFIX_GOOD_MOOD = [
+    "Tot ziens xoxo",
+    ":D",
+    "Haha",
+    "Even koffie, zo terug!",
+    "Daag!",
+    "Blij u van dienst te kunnen zijn!",
+    ":)",
+    "xx",
+    "XD"
+]

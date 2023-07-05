0a316df44de99c9f4991f6e1e9a8bf7b0a255111
michaelkeesdev <63731538+michaelkeesdev@users.noreply.github.com>
2023-07-05 10:18:01 +0200

    Update riders.js

--- app/answers/riders.js
+++ app/answers/riders.js
@@ -1,1 +1,706 @@
-
+export const RIDERS = [
+  {
+    id: "1",
+    name: "Jonas Vingegaard"
+  },
+  {
+    id: "2",
+    name: "Tiesj Benoot"
+  },
+  {
+    id: "3",
+    name: "Wilco Kelderman"
+  },
+  {
+    id: "4",
+    name: "Sepp Kuss"
+  },
+  {
+    id: "5",
+    name: "Christophe Laporte"
+  },
+  {
+    id: "6",
+    name: "Wout van Aert"
+  },
+  {
+    id: "7",
+    name: "Dylan van Baarle"
+  },
+  {
+    id: "8",
+    name: "Nathan Van HooydonckUAE Emirates"
+  },
+  {
+    id: "11",
+    name: "Tadej Pogacar"
+  },
+  {
+    id: "12",
+    name: "Mikkel Bjerg"
+  },
+  {
+    id: "14",
+    name: "Felix Großschartner"
+  },
+  {
+    id: "15",
+    name: "Vegard Stake Laengen"
+  },
+  {
+    id: "16",
+    name: "Rafal Majka"
+  },
+  {
+    id: "17",
+    name: "Marc Soler"
+  },
+  {
+    id: "18",
+    name: "Matteo Trentin"
+  },
+  {
+    id: "19",
+    name: "Adam YatesINEOS Grenadiers"
+  },
+  {
+    id: "21",
+    name: "Egan Bernal"
+  },
+  {
+    id: "22",
+    name: "Jonathan Castroviejo"
+  },
+  {
+    id: "23",
+    name: "Omar Fraile"
+  },
+  {
+    id: "24",
+    name: "Michal Kwiatkowski"
+  },
+  {
+    id: "25",
+    name: "Daniel Felipe Martíne"
+  },
+  {
+    id: "26",
+    name: "Tom Pidcock"
+  },
+  {
+    id: "27",
+    name: "Carlos Rodriguez"
+  },
+  {
+    id: "28",
+    name: "Ben TurnerGroupama-FDJ"
+  },
+  {
+    id: "31",
+    name: "David Gaudu"
+  },
+  {
+    id: "32",
+    name: "Kevin Geniets"
+  },
+  {
+    id: "33",
+    name: "Stefan Küng"
+  },
+  {
+    id: "34",
+    name: "Olivier Le Gac"
+  },
+  {
+    id: "35",
+    name: "Valentin Madouas"
+  },
+  {
+    id: "36",
+    name: "Quentin Pacher"
+  },
+  {
+    id: "37",
+    name: "Thibaut Pinot"
+  },
+  {
+    id: "38",
+    name: "Lars van den BergEF Education-EasyPost"
+  },
+  {
+    id: "41",
+    name: "Richard Carapaz"
+  },
+  {
+    id: "42",
+    name: "Andrey Amador"
+  },
+  {
+    id: "43",
+    name: "Alberto Bettiol"
+  },
+  {
+    id: "44",
+    name: "Esteban Chaves"
+  },
+  {
+    id: "45",
+    name: "Magnus Cort"
+  },
+  {
+    id: "46",
+    name: "Neilson Powless"
+  },
+  {
+    id: "47",
+    name: "James Shaw"
+  },
+  {
+    id: "48",
+    name: "Rigoberto UranSoudal-QuickStep"
+  },
+  {
+    id: "51",
+    name: "Julian Alaphilippe"
+  },
+  {
+    id: "52",
+    name: "Kasper Asgreen"
+  },
+  {
+    id: "53",
+    name: "Rémi Cavagna"
+  },
+  {
+    id: "54",
+    name: "Tim Declercq"
+  },
+  {
+    id: "55",
+    name: "Dries Devenyns"
+  },
+  {
+    id: "56",
+    name: "Fabio Jakobsen"
+  },
+  {
+    id: "57",
+    name: "Yves Lampaert"
+  },
+  {
+    id: "58",
+    name: "Michael Mørkøv, ,Bahrain Victorious"
+  },
+  {
+    id: "62",
+    name: "Mikel Landa"
+  },
+  {
+    id: "63",
+    name: "Nikias Arndt"
+  },
+  {
+    id: "64",
+    name: "Phil Bauhaus"
+  },
+  {
+    id: "65",
+    name: "Pello Bilbao"
+  },
+  {
+    id: "66",
+    name: "Jack Haig"
+  },
+  {
+    id: "67",
+    name: "Matej Mohoric"
+  },
+  {
+    id: "68",
+    name: "Wout Poels"
+  },
+  {
+    id: "69",
+    name: "Fred WrightBORA-hansgrohe"
+  },
+  {
+    id: "71",
+    name: "Jai Hindley"
+  },
+  {
+    id: "72",
+    name: "Emanuel Buchmann"
+  },
+  {
+    id: "73",
+    name: "Marco Haller"
+  },
+  {
+    id: "74",
+    name: "Bob Jungels"
+  },
+  {
+    id: "75",
+    name: "Patrick Konrad"
+  },
+  {
+    id: "76",
+    name: "Jordi Meeus"
+  },
+  {
+    id: "77",
+    name: "Nils Politt"
+  },
+  {
+    id: "78",
+    name: "Danny van PoppelLidl-Trek"
+  },
+  {
+    id: "81",
+    name: "Giulio Ciccone"
+  },
+  {
+    id: "82",
+    name: "Tony Gallopin"
+  },
+  {
+    id: "83",
+    name: "Mattias Skjelmose"
+  },
+  {
+    id: "84",
+    name: "Alex Kirsch"
+  },
+  {
+    id: "85",
+    name: "Juan Pedro López"
+  },
+  {
+    id: "86",
+    name: "Mads Pedersen"
+  },
+  {
+    id: "87",
+    name: "Quinn Simmons"
+  },
+  {
+    id: "88",
+    name: "Jasper StuyvenAG2R Citroën"
+  },
+  {
+    id: "91",
+    name: "Ben O’Connor"
+  },
+  {
+    id: "92",
+    name: "Clément Berthet"
+  },
+  {
+    id: "93",
+    name: "Benoît Cosnefroy"
+  },
+  {
+    id: "94",
+    name: "Stan Dewulf"
+  },
+  {
+    id: "95",
+    name: "Felix Gall"
+  },
+  {
+    id: "96",
+    name: "Oliver Naesen"
+  },
+  {
+    id: "97",
+    name: "Aurélien Paret-Peintre"
+  },
+  {
+    id: "98",
+    name: "Nans PetersAlpecin – Deceuninck"
+  },
+  {
+    id: "101",
+    name: "Mathieu van der Poel"
+  },
+  {
+    id: "102",
+    name: "Silvan Dillier"
+  },
+  {
+    id: "103",
+    name: "Michael Gogl"
+  },
+  {
+    id: "104",
+    name: "Quinten Hermans"
+  },
+  {
+    id: "105",
+    name: "Søren Kragh Andersen"
+  },
+  {
+    id: "106",
+    name: "Jasper Philipsen"
+  },
+  {
+    id: "107",
+    name: "Jonas Rickaert"
+  },
+  {
+    id: "108",
+    name: "Ramon SinkeldamIntermarché-Circus-Wanty"
+  },
+  {
+    id: "111",
+    name: "Biniam Girmay"
+  },
+  {
+    id: "112",
+    name: "Lilian Calmejane"
+  },
+  {
+    id: "113",
+    name: "Rui Costa"
+  },
+  {
+    id: "114",
+    name: "Louis Meintjes"
+  },
+  {
+    id: "115",
+    name: "Adrien Petit"
+  },
+  {
+    id: "116",
+    name: "Dion Smith"
+  },
+  {
+    id: "117",
+    name: "Mike Teunissen"
+  },
+  {
+    id: "118",
+    name: "Georg ZimmermannCofidis"
+  },
+  {
+    id: "121",
+    name: "Guillaume Martin"
+  },
+  {
+    id: "122",
+    name: "Bryan Coquard"
+  },
+  {
+    id: "123",
+    name: "Simon Geschke"
+  },
+  {
+    id: "124",
+    name: "Ion Izagirre"
+  },
+  {
+    id: "125",
+    name: "Victor Lafay"
+  },
+  {
+    id: "126",
+    name: "Anthony Perez"
+  },
+  {
+    id: "127",
+    name: "Alexis Renard"
+  },
+  {
+    id: "128",
+    name: "Axel ZingleMovistar"
+  },
+  {
+    id: "131",
+    name: "Enric Mas"
+  },
+  {
+    id: "132",
+    name: "Ruben Guerreiro"
+  },
+  {
+    id: "133",
+    name: "Alex Aranburu"
+  },
+  {
+    id: "134",
+    name: "Gorka Izagirre"
+  },
+  {
+    id: "135",
+    name: "Matteo Jorgenson"
+  },
+  {
+    id: "136",
+    name: "Gregor Mühlberger"
+  },
+  {
+    id: "137",
+    name: "Nelson Oliveira"
+  },
+  {
+    id: "138",
+    name: "Antonio PedreroTeam dsm-firmenich"
+  },
+  {
+    id: "141",
+    name: "Romain Bardet"
+  },
+  {
+    id: "142",
+    name: "John Degenkolb"
+  },
+  {
+    id: "143",
+    name: "Matthew Binham"
+  },
+  {
+    id: "144",
+    name: "Alex Edmondson"
+  },
+  {
+    id: "145",
+    name: "Nils Eekhoff"
+  },
+  {
+    id: "146",
+    name: "Chris Hamilton"
+  },
+  {
+    id: "147",
+    name: "Kevin Vermaerke"
+  },
+  {
+    id: "148",
+    name: "Sam WelsfordIsrael-Premier Tech"
+  },
+  {
+    id: "151",
+    name: "Michael Woods"
+  },
+  {
+    id: "152",
+    name: "Guillaume Bouvin"
+  },
+  {
+    id: "153",
+    name: "Simon Clarke"
+  },
+  {
+    id: "154",
+    name: "Hugo Houle"
+  },
+  {
+    id: "155",
+    name: "Krists Neilands"
+  },
+  {
+    id: "156",
+    name: "Nick Schultz"
+  },
+  {
+    id: "157",
+    name: "Corbin Strong"
+  },
+  {
+    id: "158",
+    name: "Dylan TeunsTeam Jayco AlUla"
+  },
+  {
+    id: "161",
+    name: "Simon Yates"
+  },
+  {
+    id: "162",
+    name: "Lawson Craddock"
+  },
+  {
+    id: "163",
+    name: "Luke Durbridge"
+  },
+  {
+    id: "164",
+    name: "Dylan Groenewegen"
+  },
+  {
+    id: "165",
+    name: "Chris Harper"
+  },
+  {
+    id: "166",
+    name: "Christopher Juul-Jensen"
+  },
+  {
+    id: "167",
+    name: "Luka Mezgec"
+  },
+  {
+    id: "168",
+    name: "Elmar ReindersArkéa Samsic"
+  },
+  {
+    id: "171",
+    name: "Warren Barguil"
+  },
+  {
+    id: "172",
+    name: "Jenthe Biermans"
+  },
+  {
+    id: "173",
+    name: "Clément Champoussin"
+  },
+  {
+    id: "174",
+    name: "Anthony Delaplace"
+  },
+  {
+    id: "175",
+    name: "Simon Gugliemi"
+  },
+  {
+    id: "176",
+    name: "Matis Louvel"
+  },
+  {
+    id: "177",
+    name: "Luca Mozzato"
+  },
+  {
+    id: "178",
+    name: "Laurent PichonJasper StuyvenLotto-Dstny"
+  },
+  {
+    id: "181",
+    name: "Caleb Ewan"
+  },
+  {
+    id: "182",
+    name: "Victor Campenaerts"
+  },
+  {
+    id: "183",
+    name: "Jasper De Buyst"
+  },
+  {
+    id: "184",
+    name: "Pascal Eenkhoorn"
+  },
+  {
+    id: "185",
+    name: "Frederik Frison"
+  },
+  {
+    id: "186",
+    name: "Jacopo Guarnieri"
+  },
+  {
+    id: "187",
+    name: "Maxim Van Gils"
+  },
+  {
+    id: "188",
+    name: "Florian VermeerschAstana Qazaqstan Team"
+  },
+  {
+    id: "191",
+    name: "Mark Cavendish"
+  },
+  {
+    id: "192",
+    name: "Cees Bol"
+  },
+  {
+    id: "193",
+    name: "David de la Cruz"
+  },
+  {
+    id: "194",
+    name: "Yevgeniy Fedorov"
+  },
+  {
+    id: "195",
+    name: "Alexey Loetsenko"
+  },
+  {
+    id: "196",
+    name: "Gianni Moscon"
+  },
+  {
+    id: "197",
+    name: "Luis León Sánchez"
+  },
+  {
+    id: "198",
+    name: "Harold TejadaUno-X"
+  },
+  {
+    id: "201",
+    name: "Alexander Kristoff"
+  },
+  {
+    id: "202",
+    name: "Jonas Abrahamsen"
+  },
+  {
+    id: "203",
+    name: "Anthon Charmig"
+  },
+  {
+    id: "204",
+    name: "Tobias Halland Johannessen"
+  },
+  {
+    id: "205",
+    name: "Rasmus Tiller"
+  },
+  {
+    id: "206",
+    name: "Torstein Træen"
+  },
+  {
+    id: "207",
+    name: "Søren Wærenskjold"
+  },
+  {
+    id: "208",
+    name: "Jonas GregaardTotalEnergies"
+  },
+  {
+    id: "211",
+    name: "Peter Sagan"
+  },
+  {
+    id: "212",
+    name: "Edvald Boasson Hagen"
+  },
+  {
+    id: "213",
+    name: "Mathieu Burgaudeau"
+  },
+  {
+    id: "214",
+    name: "Steff Cras"
+  },
+  {
+    id: "215",
+    name: "Valentin Ferron"
+  },
+  {
+    id: "216",
+    name: "Pierre Latour"
+  },
+  {
+    id: "217",
+    name: "Daniel Oss"
+  },
+  {
+    id: "218",
+    name: "Anthony Turgis"
+  }
+];

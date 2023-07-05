3352fe2a60f82c9577d7eb445b53548b0dc68bbe
michaelkeesdev <63731538+michaelkeesdev@users.noreply.github.com>
2023-07-05 10:20:31 +0200

    Update TourService.js

--- app/services/tv/TourService.js
+++ app/services/tv/TourService.js
@@ -1,1 +1,10 @@
+import { sample } from "lodash";
+import { RIDERS } from "./../../answers/riders";
 
+class TourService {
+  giveRandomName() {
+    return sample(RIDERS);
+  }
+}
+
+export { TourService };
